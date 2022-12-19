4f2516576fdfd4321afe69ecd146150dbbeb3e04
Shaham Noorani <shahamnoorani@gmail.com>
2022-12-19 11:46:44 -0600

    remove sqlize

--- api/app.ts
+++ api/app.ts
@@ -1,23 +1,21 @@
-import "reflect-metadata";
-import express, { Request, Response } from "express";
-import connection from "./db/connection";
-import router from "./router";
+import 'reflect-metadata';
+import express, { Request, Response } from 'express';
+import router from './router';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", router);
+app.use('/api', router);
 
-app.get("/", (req: Request, res: Response): Response => {
-  return res.json({ message: "It works!" });
+app.get('/', (req: Request, res: Response): Response => {
+  return res.json({ message: 'It works!' });
 });
 
 const start = async (): Promise<void> => {
   try {
-    await connection.sync();
     app.listen(3000, () => {
-      console.log("Server started at http://localhost:3000");
+      console.log('Server started at http://localhost:3000');
     });
   } catch (error) {
     console.error(error);
